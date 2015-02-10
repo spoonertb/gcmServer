006fbbf4ebc5c090eeaf243b10a868359fb9aeb2
Tom <spoonertb@gmail.com>
2015-02-09 20:34:26 -0700

    Adding framework for gcmServer

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "gcm");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "BIzaSyCRLa4LQZWNQBcRCYcIVYA45i9i8zfClqc"); // Place your Google API Key
+?>
